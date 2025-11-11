dd91fa52ebd77d59fdae6768338181e5653f1cfb
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-11-11 22:06:56 +0100

    add cool ripple effect background

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -5,15 +5,18 @@ import { Header } from "@/components/home/header";
 import { Main, AI_SOCIETY_ORGANIZATIONS } from "@/components/home/main";
 import { Footer } from "@/components/home/footer";
 import { Background } from "@/components/ui/background";
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
 
 export function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const currentColor = AI_SOCIETY_ORGANIZATIONS[currentIndex].color;
 
   return (
-    <div className="relative min-h-[100dvh] flex flex-col">
+    <div className="relative min-h-dvh flex flex-col overflow-hidden">
       <Background color={currentColor} />
-      <div className="relative z-10 flex flex-col min-h-[100dvh]">
+      
+      <div className="relative z-10 flex flex-col min-h-dvh">
+      <BackgroundRippleEffect />
         <Header />
         <Main currentIndex={currentIndex} onIndexChange={setCurrentIndex} />
         <Footer />
