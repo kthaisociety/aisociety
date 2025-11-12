7fc0f2f6f8ec91fc3faf886b30c0e61c58505e57
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-11-12 13:58:55 +0100

    change text

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -14,7 +14,7 @@ export function Footer() {
   return (
     <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-2 w-full max-w-7xl mx-auto px-4 py-2 sm:py-3 shrink-0 z-10">
       <TextScramble as="span" className="text-xs sm:text-sm font-medium font-martian-mono text-gray-900 [text-shadow:0_0_12px_rgb(255_255_255/90%),0_0_6px_rgb(255_255_255/80%)]">
-        {`© ${getCurrentYear()} (AI Society)`}
+        {`© ${getCurrentYear()} (AIS)`}
       </TextScramble>
       <TextScramble as="span" className="text-xs sm:text-sm font-medium font-martian-mono text-gray-900 [text-shadow:0_0_12px_rgb(255_255_255/90%),0_0_6px_rgb(255_255_255/80%)] hidden sm:inline">|</TextScramble>
       <span onClick={handleEmailClick} className="cursor-pointer">
